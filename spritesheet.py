405b9a7f3337b2480d0714ec918af27b77e687e6
Drvanon <robbiedorstijn@gmail.com>
2013-02-16 18:25:18 +0100

    Make more readable version of spritesheet.py

--- spritesheet.py
+++ spritesheet.py
@@ -4,16 +4,17 @@
 # I've added some code to fail if the file wasn't found..
 # Note: When calling images_at the rect is the format:
 # (x, y, x + offset, y + offset)
- 
-import pygame
- 
-class spritesheet(object):
+
+import pygame, sys
+
+class Spritesheet(object):
     def __init__(self, filename):
         try:
-            self.sheet = pygame.image.load(filename).convert()
+            self.sheet = pygame.image.load(filename)
         except pygame.error, message:
             print 'Unable to load spritesheet image:', filename
-            raise SystemExit, message
+            sys.exit(1)
+
     # Load a specific image from a specific rectangle
     def image_at(self, rectangle, colorkey = None):
         "Loads image from x,y,x+offset,y+offset"
@@ -25,10 +26,12 @@ class spritesheet(object):
                 colorkey = image.get_at((0,0))
             image.set_colorkey(colorkey, pygame.RLEACCEL)
         return image
+
     # Load a whole bunch of images and return them as a list
     def images_at(self, rects, colorkey = None):
         "Loads multiple images, supply a list of coordinates" 
         return [self.image_at(rect, colorkey) for rect in rects]
+
     # Load a whole strip of images
     def load_strip(self, rect, image_count, colorkey = None):
         "Loads a strip of images and returns them as a list"
